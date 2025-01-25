8fe70aae0ba823fe09c5fa32033ffef543cc44c5
PIGSG <sma8323@gmail.com>
2025-01-25 12:15:36 +0900

    upload tests

---
+++ mysite/accounts/views.py
@@ -0,0 +1,189 @@
+import random
+import string
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from .forms import AccountDeleteForm, UsernameFindForm, LoginForm, PasswordResetForm, RegisterForm, PasswordUpdateForm, ProfileUpdateForm
+
+# 회원가입
+def register_account(request):
+    if request.user.is_authenticated:
+        return redirect('auth:profile')
+
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password1"],
+                first_name=form.cleaned_data["first_name"],
+                email=form.cleaned_data["email"]
+            )
+            user.save()
+            messages.success(request, '회원가입이 완료되었습니다.')
+            return redirect('auth:login')
+        else:
+            messages.error(request, '회원가입에 실패했습니다.')
+
+    return render(request, 'accounts/signup.html', {'form': form, 'message_class': 'col-4 mx-auto'})
+
+# 로그인
+def login_account(request):
+    if request.user.is_authenticated:
+        return redirect('auth:profile')
+
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            # 로그인 실패
+            if user is None:
+                messages.error(request, '아이디 또는 비밀번호가 일치하지 않습니다.')
+                return redirect('auth:login')
+
+            # 로그인 성공
+            login(request, user)
+            return redirect('auth:profile')
+        else:
+            messages.error(request, '아이디 또는 비밀번호를 입력해주세요.')
+
+    return render(request, 'accounts/login.html', {'form': form, 'message_class': 'col-4 mx-auto'})
+
+# 로그아웃
+def logout_account(request):
+    logout(request)
+    return redirect('auth:login')
+
+# 프로필 보기
+@login_required(login_url='auth:login')
+def get_profile(request):
+    return render(request, 'accounts/profile.html', {'message_class': 'col-4 mx-auto'})
+
+# 프로필 수정
+@login_required(login_url='auth:login')
+def update_profile(request):
+    form = ProfileUpdateForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, '프로필 수정이 완료되었습니다.')
+            return redirect('auth:profile')
+        else:
+            messages.error(request, '프로필 수정에 실패했습니다.')
+
+    return render(request, 'auth/update_profile.html', {'form': form, 'message_class': 'col-4 mx-auto'})
+
+# 비밀번호 수정
+@login_required(login_url='auth:login')
+def update_password(request):
+    form = PasswordUpdateForm()
+
+    if request.method == 'POST':
+        form = PasswordUpdateForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            password1 = form.cleaned_data['password1']
+
+            user = authenticate(request, username=request.user.username, password=password)
+            if user is None:
+                messages.error(request, '기존 비밀번호가 일치하지 않습니다.')
+                return redirect('auth:update_password')
+
+            user = request.user
+            user.set_password(password1)
+            user.save()
+
+            logout(request)
+            messages.success(request, '비밀번호 수정이 완료되었습니다. 다시 로그인해주세요.')
+            return redirect('auth:login')
+        else:
+            messages.error(request, '비밀번호 수정에 실패했습니다.')
+
+    return render(request, 'accounts/update_password.html', {'form': form, 'message_class': 'col-4 mx-auto'})
+
+# 아이디 찾기
+def find_username(request):
+    if request.user.is_authenticated:
+        return redirect('auth:profile')
+
+    form = UsernameFindForm()
+
+    if request.method == 'POST':
+        form = UsernameFindForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            email = form.cleaned_data['email']
+            user = User.objects.filter(first_name=first_name, email=email).first()
+            if user:
+                messages.success(request, f'아이디는 {user.username}입니다.')
+                return redirect('auth:login')
+            else:
+                messages.error(request, '아이디 찾기에 실패했습니다.')
+        else:
+            messages.error(request, '아이디 찾기에 실패했습니다.')
+
+    return render(request, 'accounts/find_username.html', {'form': form, 'message_class': 'col-4 mx-auto'})
+
+# 비밀번호 초기화
+def reset_password(request):
+    if request.user.is_authenticated:
+        return redirect('auth:profile')
+
+    form = PasswordResetForm()
+
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            user = User.objects.filter(first_name=first_name, username=username, email=email).first()
+            if user:
+                password = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+                user.set_password(password)
+                user.save()
+
+                messages.success(request, f'비밀번호가 초기화되었습니다. 새로운 비밀번호는 {password}입니다.')
+                return redirect('auth:login')
+            else:
+                messages.error(request, '비밀번호 초기화에 실패했습니다.')
+
+    return render(request, 'accounts/reset_password.html', {'form': form, 'message_class': 'col-4 mx-auto'})
+
+# 탈퇴
+@login_required(login_url='auth:login')
+def delete_account(request):
+    form = AccountDeleteForm()
+
+    if request.method == 'POST':
+        form = AccountDeleteForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = User.objects.filter(first_name=first_name, username=username, email=email).first()
+            authenticated = authenticate(request, username=username, password=password)
+            if user and authenticated is not None:
+                user.delete()
+                logout(request)
+                messages.success(request, '회원탈퇴가 완료되었습니다.')
+                return redirect('auth:login')
+            else:
+                messages.error(request, '회원탈퇴에 실패했습니다. 입력정보를 확인하세요.')
+
+    return render(request, 'accounts/delete_account.html', {'form': form, 'message_class': 'col-4 mx-auto'})
---
+++ mysite/comments/views.py
@@ -0,0 +1,127 @@
+from django.http import JsonResponse
+from django.utils import timezone
+
+from posts.models import Post
+from .models import Comment
+
+# 댓글 목록
+def get_comments(post_id):
+    comments = Comment.objects.filter(post=post_id).order_by('-created_at')
+    comments_list = []
+    for comment in comments:
+        comments_list.append({
+            'id': comment.id,
+            'content': comment.content,
+            'created_by': comment.created_by.first_name,
+            'updated_by': comment.updated_by.first_name,
+            'created_at': comment.created_at.astimezone(timezone.get_current_timezone()).strftime('%Y-%m-%d %H:%M'),
+            'updated_at': comment.updated_at.astimezone(timezone.get_current_timezone()).strftime('%Y-%m-%d %H:%M'),
+        })
+    return comments_list
+
+# 댓글 등록
+def create_comment(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({'result': 'error', 'message': '로그인 후 이용해주세요.'})
+    
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        if not post_id:
+            return JsonResponse({'result': 'error', 'message': '게시글을 선택해주세요.'})
+        
+        post = Post.objects.get(id=post_id)        
+        if not post:
+            return JsonResponse({'result': 'error', 'message': '게시글을 찾을 수 없습니다.'})
+        
+        content = request.POST.get('content')          
+        if not content:
+            return JsonResponse({'result': 'error', 'message': '댓글 내용을 입력해주세요.'})
+        
+        # 댓글 등록
+        comment = Comment(
+            post=post,
+            content=content,
+            created_by=request.user,
+            updated_by=request.user
+        )
+        comment.save()
+        
+        # 댓글 목록
+        comments = get_comments(post_id)
+
+        return JsonResponse({'result': 'success', 'comments': comments})
+
+# 댓글 수정
+def update_comment(request, comment_id):
+    if not request.user.is_authenticated:
+        return JsonResponse({'result': 'error', 'message': '로그인 후 이용해주세요.'})
+    
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        if not post_id:
+            return JsonResponse({'result': 'error', 'message': '게시글을 선택해주세요.'})
+        
+        post = Post.objects.get(id=post_id)        
+        if not post:
+            return JsonResponse({'result': 'error', 'message': '게시글을 찾을 수 없습니다.'})
+        
+        content = request.POST.get('content')          
+        if not content:
+            return JsonResponse({'result': 'error', 'message': '댓글 내용을 입력해주세요.'})
+        
+        comment_id = request.POST.get('comment_id')
+        if not comment_id:
+            return JsonResponse({'result': 'error', 'message': '댓글을 선택해주세요.'})
+        
+        comment = Comment.objects.get(id=comment_id)
+        if not comment:
+            return JsonResponse({'result': 'error', 'message': '댓글을 찾을 수 없습니다.'})
+        
+        if comment.created_by != request.user:
+            return JsonResponse({'result': 'error', 'message': '댓글 수정 권한이 없습니다.'})
+        
+        # 댓글 수정
+        comment.content = content
+        comment.updated_by = request.user
+        comment.save()
+
+        # 댓글 목록
+        comments = get_comments(post_id)
+
+        return JsonResponse({'result': 'success', 'comments': comments})
+
+
+
+def delete_comment(request, comment_id):
+    if not request.user.is_authenticated:
+        return JsonResponse({'result': 'error', 'message': '로그인 후 이용해주세요.'})
+
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')  # AJAX 요청에서 넘어오는 데이터 확인
+        if not post_id:
+            return JsonResponse({'result': 'error', 'message': '게시글을 선택해주세요.'})
+
+        # 게시글 확인
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return JsonResponse({'result': 'error', 'message': '게시글을 찾을 수 없습니다.'})
+
+        # 댓글 확인
+        try:
+            comment = Comment.objects.get(id=comment_id)
+        except Comment.DoesNotExist:
+            return JsonResponse({'result': 'error', 'message': '댓글을 찾을 수 없습니다.'})
+
+        # 댓글 작성자 확인
+        if comment.created_by != request.user:
+            return JsonResponse({'result': 'error', 'message': '댓글 삭제 권한이 없습니다.'})
+
+        # 댓글 삭제
+        comment.delete()
+
+        # 최신 댓글 목록 반환
+        comments = get_comments(post_id)
+        return JsonResponse({'result': 'success', 'comments': comments})
+
+    return JsonResponse({'result': 'error', 'message': '잘못된 요청입니다.'})
